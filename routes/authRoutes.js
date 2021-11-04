abfb34ab94789ab2dc518ba90ba5967daaba88b4
OsmanBytyqi <obytyqi2001@gmail.com>
2021-11-03 21:46:22 -0700

    adding login and signup views

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,8 +4,8 @@ const authController= require("../controllers/authController");
 
 const router =Router();
 
-router.get('/singup',authController.singup_get);
-router.post('/singup',authController.singup_post);
+router.get('/signup',authController.signup_get);
+router.post('/signup',authController.signup_post);
 router.get('/login',authController.login_get);
 router.post('/login',authController.login_post);
 
