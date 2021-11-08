ea9be04f78dcf3adb4bd3ef89e7acd374e9e49c5
OsmanBytyqi <obytyqi2001@gmail.com>
2021-11-08 22:00:18 +0100

    created jwt

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 const connstr = "mongodb+srv://osmani:osmani1.@mani.5y7yi.mongodb.net/node-auth?retryWrites=true&w=majority";
 
 // database connection
-
+// 
 mongoose.connect(connstr, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3000))
   .then(result=> console.log('your server is running at http://localhost:3000'))
@@ -27,25 +27,5 @@ app.get('/smoothies', (req, res) => res.render('smoothies'));
 
 app.use(authRoutes);
 
-//cookies
 
 
-app.get('/set-cookies', (req, res) => {
-
-  // res.setHeader('Set-Cookie', 'newUser=true');
-  
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
-
-  res.send('you got the cookies!');
-
-});
-
-app.get('/read-cookies', (req, res) => {
-
-  const cookies = req.cookies;
-  console.log(cookies.newUser);
-
-  res.json(cookies);
-
-});
