387e22225cbb4a3209855814cfe82a1db0b5b572
OsmanBytyqi <obytyqi2001@gmail.com>
2021-11-26 23:13:42 +0100

    login part

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,7 @@ const handleErrors = (err) => {
 
 
 
-=======
+//=======
   /// jsonwebtoken
 
   const maxAge = 3 * 24 * 60 * 60;
@@ -53,7 +53,7 @@ const signup_post= async (req, res) => {
   const { email, password } = req.body;
 
 
-     const { email, password } = req.body;
+     //const { email, password } = req.body;
 
     try {
       const user = await User.create({ email, password });
@@ -73,9 +73,12 @@ const signup_post= async (req, res) => {
 
 const login_post=async (req,res)=>{
     const {email,password}=req.body;
-    console.log(email,password)
-
-    res.send(" user login");
+    try {
+      const user = await User.login(email, password);
+      res.status(200).json({ user: user._id });
+    } catch (err) {
+      res.status(400).json({});
+    }
 }
 
 
