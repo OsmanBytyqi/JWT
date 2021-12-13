64e899a63a9dca3c584786fcdf4e8ddf258ce67f
OsmanBytyqi <obytyqi2001@gmail.com>
2021-12-14 00:13:59 +0100

    logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,6 +9,7 @@ router.post('/signup',authController.signup_post);
 router.get('/login',authController.login_get);
 router.post('/login',authController.login_post);
 
+router.get('/logout',authController.logout_get);
 
 module.exports=router;
 
